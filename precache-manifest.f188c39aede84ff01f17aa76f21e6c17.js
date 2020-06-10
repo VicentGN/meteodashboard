edf94a9767d30290b69afc89d3c98a7725de194f
VicentGN <vicentgarcian@gmail.com>
2020-06-10 09:30:24 +0200

    Updates

---
+++ precache-manifest.f188c39aede84ff01f17aa76f21e6c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4eba5838e6720f140d6d4ee5a835d7a",
+    "url": "/meteodashboard/index.html"
+  },
+  {
+    "revision": "bd4697cd71ea314aed80",
+    "url": "/meteodashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee2dfe51519d9973adc4",
+    "url": "/meteodashboard/static/js/2.c362d982.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/meteodashboard/static/js/2.c362d982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4697cd71ea314aed80",
+    "url": "/meteodashboard/static/js/main.0c4881f6.chunk.js"
+  },
+  {
+    "revision": "70c620a28805158eb502",
+    "url": "/meteodashboard/static/js/runtime-main.6a576051.js"
+  }
+]);
